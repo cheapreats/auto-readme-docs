ad5136af097e383165aa898ef46e27f83d6c5119
Nick Chaloult <npchaloult@gmail.com>
2020-06-03 16:59:54 -0400

    Add MarkdownDisplayLine component

--- src/components/MarkdownDisplay.tsx
+++ src/components/MarkdownDisplay.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import MarkdownDisplayLine from './MarkdownDisplayLine';
 
-// Component
+// Styles
+
+const Card = styled.div`
+  width: 100%;
+  padding: 2rem;
+  background: #212428;
+  color: white;
+`;
+
+// Components
 
 interface Props {
   content: string[];
 }
 
 const MarkdownDisplay: React.FC<Props> = (props: Props) => (
-  <>
-    <p>MarkdownDisplay component</p>
-    {props.content.map((line) => <p>{line}</p>)}
-  </>
+  <Card>
+    {
+      props.content.map((line, i) => (
+        <MarkdownDisplayLine isOddNumberedLine={i % 2 === 1} content={line} />
+      ))
+    }
+  </Card>
 );
 
 export default MarkdownDisplay;
