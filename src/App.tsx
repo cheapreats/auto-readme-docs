a64563bab4f6b0a1939236ededeefe89a1c5e267
ralph-dev <ralphpal@hotmail.com>
2020-06-03 11:19:56 -0400

    Added MarkDown Box Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,67 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
-const App: React.FC = () => {
-  return <Input />;
-};
+const App: React.FC = () => <Input />;
 
 const Input: React.FC = () => {
   const [url, setUrl] = useState(
-    'Enter a Github Repo'
+    'Enter a Github Repo',
   );
 
-  const handleChange = event => setUrl(event.target.value);
+  const handleChange = (event) => setUrl(event.target.value);
 
   return (
-    <div>
+    <Container>
       <h1>SWEGGG</h1>
-      <input type="text" value={url} onChange={handleChange}/>
-    </div>
+      <input type="text" value={url} onChange={handleChange} />
+        <MarkDownDisplay>
+            <MarkDownText>Lorem Ipmsum</MarkDownText>
+            <Copy>
+                <CopyButton>
+                    <CopyText>Copy</CopyText>
+                </CopyButton>
+            </Copy>
+        </MarkDownDisplay>
+    </Container>
   );
 };
 
 export default App;
+
+const MarkDownText = styled.p`
+  font-family: Arial;
+  font-size: 24px;
+`;
+
+const Container = styled.div`
+    background-color: aquamarine;
+`;
+
+const MarkDownDisplay = styled.div`
+    width: 300px;
+    flex: 1 0;
+    background-color: grey;
+    box-shadow: 0px 10px 5px lightgray;
+    border-radius: 7px;
+`;
+
+const Copy = styled.div`
+  height: 25px;
+  background-color: dimgrey;
+  border-bottom-left-radius: 7px;
+  border-bottom-right-radius: 7px;
+`;
+const CopyButton = styled.div`
+  height: 25px;
+  margin-left: auto;
+  background-color: darkgray;
+  text-align: center;
+  width: 50px;
+  border-bottom-right-radius: 7px;
+`;
+
+const CopyText = styled.p`
+  font-family: Arial;
+  font-size: 20px;
+  color: white;
+`;
