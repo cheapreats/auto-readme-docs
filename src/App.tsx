3b1880d1b017fd292583fd3c74d8955af0c6829a
Mohammed Sahl <sahl.kably@gmail.com>
2020-06-04 11:59:16 -0400

    Add go button

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,49 @@ const Column = styled.div`
   flex-direction: column;
 `;
 
+const InputText = styled.input.attrs(props => ({
+  type: 'text',
+  placeholder:'Enter a URL',
+}))`
+  padding: 0.5em;
+  border-radius: 3px;
+  border: 1px solid black;
+  display: block;
+  margin: 0 0 3px 0;
+
+  ::placeholder {
+    color: Grey;
+  }
+`;
+
+const InputButton = styled.input.attrs(props => ({
+  type: 'submit',
+  value: "Go"
+}))`
+  position: relative;
+  top: 0;
+  border-radius: 3px;
+  border: 1px solid black;
+  margin: 2px 2px 2px 0;
+  padding: 3px;
+  color: #212428;
+  background: #fdce03;
+
+  transition: all 200ms;
+
+  &:hover {
+    cursor: pointer;
+
+    top: -4px;
+    box-shadow: 0px 3px 0px -1px #555555;
+  }
+
+  $:active {
+    top: 0;
+    box-shadow: none;
+  }
+`;
+
 // Components
 
 const App: React.FC = () => {
@@ -30,7 +73,8 @@ const App: React.FC = () => {
   const handleURLChange = (e: React.ChangeEvent<HTMLInputElement>) => setURL(e.target.value);
 
   const handleKeyPressed = async event => {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' || event.type === 'click') {
+      event.preventDefault();
       // Expecting a URL like 'https://github.com/${owner}/${repo}'
       let pathArray = url.split('/');
       let owner = pathArray[3];
@@ -56,7 +100,8 @@ const App: React.FC = () => {
       <Column>
         <h1>SWEGGG</h1>
         <BadgesSection repoName={repoName}/>
-        <input placeholder="Enter a Github URL" type="text" value={url} onChange={handleURLChange} onKeyDown={handleKeyPressed}/>
+        <InputText onChange={handleURLChange} onKeyDown={handleKeyPressed} />
+        <InputButton onClick={handleKeyPressed}/>
         <MarkdownDisplay content={markdownDisplayContent} />
       </Column>
     </Container>
