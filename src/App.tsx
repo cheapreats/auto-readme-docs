22c3cc5caa9e43811fa13d7eb821da692562a8ef
Mohammed Sahl <32067562+mohammedsahl@users.noreply.github.com>
2020-06-04 10:36:12 -0400

    finished up the API stuff (#13)
    Wire up API calling functionality with the `MarkdownDisplay` component on the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { ripOutPaths, generateTree } from './tree'
+import { GithubAPIResponseBody } from './tree/types';
 import styled from 'styled-components';
 import MarkdownDisplay from './components/MarkdownDisplay';
 
@@ -20,16 +22,38 @@ const Column = styled.div`
 // Components
 
 const App: React.FC = () => {
-  const [url, setURL] = useState('Enter a Github Repo');
+  const [url, setURL] = useState('');
   const [markdownDisplayContent, setMarkdownDisplayContent] = useState(['public', 'src']);
 
   const handleURLChange = (e: React.ChangeEvent<HTMLInputElement>) => setURL(e.target.value);
 
+  const handleKeyPressed = async event => {
+    if (event.key === 'Enter') {
+      // Expecting a URL like 'https://github.com/${owner}/${repo}'
+      let pathArray = url.split('/');
+      let owner = pathArray[3];
+      let repo = pathArray[4];
+      await makeRequest(owner, repo);
+    }
+  }
+
+  const makeRequest = async (owner: String, repo: String) => {
+    try {
+      const res = await fetch(`https://api.github.com/repos/${owner}/${repo}/commits/master`);
+      const resJSON = await res.json();
+      const treeSHA = resJSON["commit"]["tree"]["sha"]
+      const treeRes = await fetch(`https://api.github.com/repos/${owner}/${repo}/git/trees/${treeSHA}?recursive=true`);
+      const treeJSON = await treeRes.json();
+      setMarkdownDisplayContent(generateTree(ripOutPaths(treeJSON as GithubAPIResponseBody)));
+    } catch (error) {
+      alert('Error' + error);
+    }
+  }
   return (
     <Container>
       <Column>
         <h1>SWEGGG</h1>
-        <input type="text" value={url} onChange={handleURLChange} />
+        <input placeholder="Enter a Github URL" type="text" value={url} onChange={handleURLChange} onKeyDown={handleKeyPressed}/>
         <MarkdownDisplay content={markdownDisplayContent} />
       </Column>
     </Container>
