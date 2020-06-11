680f5cc47691627f9ba335e3c1df7476c504a00c
ManpreetBhatti <manpreet@bhatti.net>
2020-06-11 16:52:02 -0400

    Update getFileTypeFromPath.ts

--- src/utils/getFileTypeFromPath.ts
+++ src/utils/getFileTypeFromPath.ts
@@ -2,22 +2,26 @@ import { FileType } from "../tree/types";
 
 /**
  * @function
- * @param path string
+ * @param {string} path
  * @returns FileType
- * @description using inputted relative GitHub path, find the FileType of the path depending on how many dots are found in the string
+ * @description using inputted relative GitHub path, get the FileType of the path depending on how many dots are found in the string
  */
 
 export const getFileTypeFromPath = (path: string): FileType => {
   // Count amount of periods assuming the path is relative and starts with "./"
   const numberOfDots = (path.match(/\./g) || []).length;
 
-  if (numberOfDots == 3) {
+  const configNumberOfDots = 3;
+  const fileNumberOfDots = 2;
+  const folderNumberOfDots = 1;
+
+  if (numberOfDots == configNumberOfDots) {
     // Length of 3 means 2 dots in the file name -- config file
     return FileType.CONFIG_FILE;
-  } else if (numberOfDots == 2) {
+  } else if (numberOfDots == fileNumberOfDots) {
     // Length of 2 means 1 dot in the file name -- regular file
     return FileType.FILE;
-  } else if (numberOfDots == 1) {
+  } else if (numberOfDots == folderNumberOfDots) {
     // Length of 1 means there are no file names in path -- folder */
     return FileType.FOLDER;
   } else {
