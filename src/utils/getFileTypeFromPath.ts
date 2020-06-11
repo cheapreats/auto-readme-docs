76a548a222f29b51871be131d70fad0a27ef3ee9
ManpreetBhatti <manpreet@bhatti.net>
2020-06-11 17:18:33 -0400

    Update getFileTypeFromPath.ts

--- src/utils/getFileTypeFromPath.ts
+++ src/utils/getFileTypeFromPath.ts
@@ -11,13 +11,17 @@ export const getFileTypeFromPath = (path: string): FileType => {
   // Count amount of periods assuming the path is relative and starts with "./"
   const numberOfDotsInPath = (path.match(/\./g) || []).length;
 
-  if (numberOfDotsInPath == 3) {
+  const configNumberOfDots = 3;
+  const fileNumberOfDots = 2;
+  const folderNumberOfDots = 1;
+
+  if (numberOfDotsInPath == configNumberOfDots) {
     // Length of 3 means 2 dots in the file name -- config file
     return FileType.CONFIG_FILE;
-  } else if (numberOfDotsInPath == 2) {
+  } else if (numberOfDotsInPath == fileNumberOfDots) {
     // Length of 2 means 1 dot in the file name -- regular file
     return FileType.FILE;
-  } else if (numberOfDotsInPath == 1) {
+  } else if (numberOfDotsInPath == folderNumberOfDots) {
     // Length of 1 means there are no file names in path -- folder */
     return FileType.FOLDER;
   } else {
