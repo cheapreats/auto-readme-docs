aec198db723d5763fb0cc893f2f0bfc0afe543f7
ManpreetBhatti <manpreet@bhatti.net>
2020-06-11 15:59:59 -0400

    Update getFileTypeFromPath.ts
    Changed count to numberOfDots

--- src/utils/getFileTypeFromPath.ts
+++ src/utils/getFileTypeFromPath.ts
@@ -1,20 +1,20 @@
 import { FileType } from "../tree/types";
 
 export const getFileTypeFromPath = (path: string): FileType => {
-  const count = (path.match(/\./g) || [])
-    .length; /* find the amount of periods assuming the path is relative and starts with "./" */
+  // Count amount of periods assuming the path is relative and starts with "./"
+  const numberOfDots = (path.match(/\./g) || []).length;
 
-  if (count == 3) {
-    /* Length of 3 means 2 dots in the file name -- config file */
+  if (numberOfDots == 3) {
+    // Length of 3 means 2 dots in the file name -- config file
     return FileType.CONFIG_FILE;
-  } else if (count == 2) {
-    /* Length of 2 means 1 dot in the file name -- regular file */
+  } else if (numberOfDots == 2) {
+    // Length of 2 means 1 dot in the file name -- regular file
     return FileType.FILE;
-  } else if (count == 1) {
-    /* Length of 1 means there are no file names in path -- folder */
+  } else if (numberOfDots == 1) {
+    // Length of 1 means there are no file names in path -- folder */
     return FileType.FOLDER;
   } else {
-    /* 0 dots or more than 3 dots means either the path is incorrect or there's an invalid file inputted */
+    // 0 dots or more than 3 dots means either the path or file invalid
     throw "Path/file not supported!";
   }
 };
