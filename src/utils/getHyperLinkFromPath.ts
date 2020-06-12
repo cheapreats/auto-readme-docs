4f85afbf3b2c3ea94762cb39d736955ece4204d1
sahandfarahani <sahandfarahani@yahoo.com>
2020-06-12 12:15:06 -0400

    deleted extra comments on jsdoc

--- src/utils/getHyperLinkFromPath.ts
+++ src/utils/getHyperLinkFromPath.ts
@@ -1,16 +1,13 @@
-/** @function getHyperLinkFromPath  generates the clickable link to a specific folder
- * @param {string} path  The path of a specific folder like "./src/components/reusable"
+/** getHyperLinkFromPath generates the clickable link to a specific folder
+ * @param {string} path  The path of a specific folder like "src/components/reusable"
  * @returns {string} the hyperLink like [reusable](./src/components/reusable)
  */
 
 export const getHyperLinkFromPath = (path: string): string => {
   const curDepth = path.match(/\//g)?.length ?? 0;
-  /** Checks to see how many '/' is included in the address */
   const deepestDirName = curDepth
     ? path.substring(path.lastIndexOf("/") + 1)
     : path;
-  /** If there are '/'s then it sets the name of the folder as the deepestDirName */
   const hyperLink = `[${deepestDirName}](./${path})`;
-  /** put the path and deepestDirName inside the hyperlink in github hyperlink format */
   return hyperLink;
 };
