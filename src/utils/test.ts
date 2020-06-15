f5f5a93981e7cf923c191df88b78d631c6ed1d3b
sahandfarahani <sahandfarahani@yahoo.com>
2020-06-14 22:27:16 -0400

    added edge case tests

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -2,8 +2,53 @@ import { expect } from "chai";
 import getHyperLinkFromPath from "./getHyperLinkFromPath";
 
 describe("getHyperLinkFromPath Function", () => {
-  it("Should change the path to github link", () => {
+  it("Should return [reusable](./src/components/reusable)", () => {
     const result = getHyperLinkFromPath("src/components/reusable");
     expect(result).to.equal("[reusable](./src/components/reusable)");
+  });
+});
+
+describe("getHyperLinkFromPath Function", () => {
+  it("Should return [reusable](./src/components/reusable)", () => {
+    const result = getHyperLinkFromPath("src");
+    expect(result).to.equal("[src](./src)");
+  });
+});
+
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath("./src")).to.throw();
+  });
+});
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath("./src/")).to.throw();
+  });
+});
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath("./src////")).to.throw();
+  });
+});
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath("/.wwqe")).to.throw();
+  });
+});
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath(".//src/")).to.throw();
+  });
+});
+
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath("src//c")).to.throw();
+  });
+});
+
+describe("getHyperLinkFromPath Function", () => {
+  it("should return Error", () => {
+    expect(() => getHyperLinkFromPath("src/")).to.throw();
   });
 });
