faca54e7e348e707241c277228796ecd03dcf745
sahandfarahani <sahandfarahani@yahoo.com>
2020-06-15 12:09:22 -0400

    added describtions for tests

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -2,33 +2,33 @@ import { expect } from "chai";
 import getHyperLinkFromPath from "./getHyperLinkFromPath";
 
 describe("getHyperLinkFromPath Function", () => {
-  it("Should return [reusable](./src/components/reusable)", () => {
+  it("test for full address : Should return [reusable](./src/components/reusable)", () => {
     const result = getHyperLinkFromPath("src/components/reusable");
     expect(result).to.equal("[reusable](./src/components/reusable)");
   });
-  it("Should return [reusable](./src/components/reusable)", () => {
+  it("test for single folder : Should return [src](./src)", () => {
     const result = getHyperLinkFromPath("src");
     expect(result).to.equal("[src](./src)");
   });
-  it("should return Error", () => {
+  it("test for relative address : should return Error", () => {
     expect(() => getHyperLinkFromPath("./src")).to.throw();
   });
-  it("should return Error", () => {
-    expect(() => getHyperLinkFromPath("./src/")).to.throw();
+  it("test for folder starting . : should return Error", () => {
+    expect(() => getHyperLinkFromPath(".src/")).to.throw();
   });
-  it("should return Error", () => {
-    expect(() => getHyperLinkFromPath("./src////")).to.throw();
+  it("test for invalid folder name : should return Error", () => {
+    expect(() => getHyperLinkFromPath("sr?c")).to.throw();
   });
-  it("should return Error", () => {
-    expect(() => getHyperLinkFromPath("/.wwqe")).to.throw();
+  it("test for address starting / : should return Error", () => {
+    expect(() => getHyperLinkFromPath("/wwqe")).to.throw();
   });
-  it("should return Error", () => {
-    expect(() => getHyperLinkFromPath(".//src/")).to.throw();
+  it("test for address engind with . : should return Error", () => {
+    expect(() => getHyperLinkFromPath("src/.")).to.throw();
   });
-  it("should return Error", () => {
+  it("test for address including // : should return Error", () => {
     expect(() => getHyperLinkFromPath("src//c")).to.throw();
   });
-  it("should return Error", () => {
+  it("test for adress ending with / : should return Error", () => {
     expect(() => getHyperLinkFromPath("src/")).to.throw();
   });
 });
