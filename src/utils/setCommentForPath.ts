a8df144b582b6db43dfe835284dbcf80386e0d8c
sahandfarahani <sahandfarahani@yahoo.com>
2020-06-16 12:50:23 -0400

    put return value for function and added more tests

--- src/utils/setCommentForPath.ts
+++ src/utils/setCommentForPath.ts
@@ -1,23 +1,22 @@
 import { TreeCore } from "../tree/types";
 /**  Replaces the existing comment by a given string
- * @param {TreeCore} treeCore the whole tree
- * @param {string} path The path of a specific file
- * @param {string} comment any string
- * @returns {void}
+ * @param {TreeCore} treeCore - the whole tree
+ * @param {string} path - The path of a specific file
+ * @param {string} comment - any string
+ * @returns {TreeCore} - New treeCore with replaced comment
  */
 
 export const setCommentForPath = (
   treeCore: TreeCore[],
   path: string,
   comment: string
-): void => {
+): TreeCore[] => {
   const pattern = /^((?![<>:"/\\|?* .])(([a-z0-9\s_@\-^!#$%&+={}\[\].]*)([/]?)))+[^/.]$/i;
   if (pattern.test(path)) {
-    const index = treeCore.findIndex((address) => address.text === path);
-
+    const index = treeCore.findIndex((core) => core.text === path);
     if (index > -1) {
-      treeCore[index].comment = " # " + comment;
-      console.log("INSIDE", treeCore);
+      treeCore[index].comment = comment ? " # " + comment : "";
+      return treeCore;
     } else {
       throw new Error("Path is not available!");
     }
--- src/utils/setCommentForPathtest.ts
+++ src/utils/setCommentForPathtest.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import setCommentForPath from "./setCommentForPath";
 import { FileType } from "../tree/types";
 
-let treeCore = [
+const treeCore = [
   {
     text: "src/utils",
     depthLevel: 2,
@@ -19,33 +19,43 @@ let treeCore = [
     text: "src/components",
     depthLevel: 2,
     fileType: FileType.FOLDER,
-    comment: "ss ",
+    comment: " # ss",
   },
 ];
 
 describe("setCommentForPath Function", () => {
-  it("full address", () => {
-    setCommentForPath(treeCore, "src", "NEW COMMENT!");
-    expect(treeCore).to.equal([
-      {
-        text: "src/utils",
-        depthLevel: 2,
-        fileType: FileType.FOLDER,
-        comment: " ",
-      },
-      {
-        text: "src",
-        depthLevel: 1,
-        fileType: FileType.FOLDER,
-        comment: " # NEW COMMENT!",
-      },
-      {
-        text: "src/components",
-        depthLevel: 2,
-        fileType: FileType.FOLDER,
-        comment: "ss ",
-      },
-    ]);
+  it("folder in root", () => {
+    const newTreeCore = setCommentForPath(treeCore, "src", "NEW COMMENT!");
+    expect(newTreeCore[1].comment).to.equal(" # NEW COMMENT!");
+  });
+  it("folder at deeper levels", () => {
+    const newTreeCore = setCommentForPath(
+      treeCore,
+      "src/components",
+      "NEW COMMENT!"
+    );
+    expect(newTreeCore[2].comment).to.equal(" # NEW COMMENT!");
+  });
+  it("folder with no comment", () => {
+    const newTreeCore = setCommentForPath(
+      treeCore,
+      "src/utils",
+      "NEW COMMENT!"
+    );
+    expect(newTreeCore[0].comment).to.equal(" # NEW COMMENT!");
+  });
+  it("deleting comment", () => {
+    const newTreeCore = setCommentForPath(treeCore, "src/utils", "");
+    expect(newTreeCore[0].comment).to.equal("");
+  });
+  it("Invalid address", () => {
+    expect(() =>
+      setCommentForPath(treeCore, "///ds//", "NEW COMMENT!")
+    ).to.throw();
+  });
+  it("Wrong address", () => {
+    expect(() =>
+      setCommentForPath(treeCore, "src/components/wrongfolder", "NEW COMMENT!")
+    ).to.throw();
   });
 });
-console.log("outside", treeCore);
