81cf97c1c3ed6b61bbc4c331723d6ea50a54945c
ManpreetBhatti <manpreet@bhatti.net>
2020-06-19 16:32:56 -0400

    Update getLargestPathLengthInLevelTest.ts
    Added more clearness to the tests

--- src/utils/getLargestPathLengthInLevelTest.ts
+++ src/utils/getLargestPathLengthInLevelTest.ts
@@ -71,27 +71,27 @@ const treeCore = [
 
 describe('Finds the largest path length in a treeCore given the depth level wanted for analysis', () => {
   // Test for depthLevel 1
-  it('should return 6', () => {
+  it('should return 6 for "public"', () => {
     expect(getLargestPathLengthInLevel(treeCore, 1)).to.equal(6);
   });
   // Test for depthLevel 2
-  it('should return 18', () => {
+  it('should return 18 for "public/favicon.ico"', () => {
     expect(getLargestPathLengthInLevel(treeCore, 2)).to.equal(18);
   });
   // Test for depthLevel 3
-  it('should return 19', () => {
+  it('should return 19 for "src/images/Demo.gif"', () => {
     expect(getLargestPathLengthInLevel(treeCore, 3)).to.equal(19);
   });
-  // Test for outOfBounds depthLevel 4
-  it('should return -1', () => {
+  // Edge case for outOfBounds depthLevel 4
+  it('should return -1 since maxDepthLevel is 3 and depthLevel is 4', () => {
     expect(getLargestPathLengthInLevel(treeCore, 4)).to.equal(-1);
   });
-  // Test for outOfBounds depthLevel 5
-  it('should return -1', () => {
+  // Edge case for outOfBounds depthLevel 5
+  it('should return -1 since maxDepthLevel is 3 and depthLevel is 5', () => {
     expect(getLargestPathLengthInLevel(treeCore, 5)).to.equal(-1);
   });
-  // Test for outOfBounds depthLevel 6
-  it('should return -1', () => {
+  // Edge case for outOfBounds depthLevel 6
+  it('should return -1 since maxDepthLevel is 3 and depthLevel is 6', () => {
     expect(getLargestPathLengthInLevel(treeCore, 6)).to.equal(-1);
   });
 });
