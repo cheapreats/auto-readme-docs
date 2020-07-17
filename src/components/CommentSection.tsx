b581fea4d13ad52bc391b54b94914e07f45d66a7
sahandfarahani <sahandfarahani@yahoo.com>
2020-07-17 12:25:30 -0400

    added setcomment feature

---
+++ src/components/CommentSection.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import styled from "styled-components";
+import { TextArea as T } from "./reusable/TextArea";
+
+const Input = styled(T)<Props>`
+  width: 100%;
+  height: 100%;
+`;
+
+const Submit = styled.button`
+  width: 27px;
+  height: 27px;
+  left: 98%;
+  top: -33px;
+  position: relative;
+  background-color: #343a40;
+  color: #b3c3d3;
+  &:focus {
+    color: yellow;
+  }
+`;
+
+const CommentBox = styled.div<Props>`
+  ${({ visible }): string =>
+    visible
+      ? `
+    display: block;
+`
+      : "display:none;"}
+`;
+
+interface Props {
+  visible: boolean;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick?: (e: MouseEvent) => void;
+}
+
+export const CommentSection: React.FC<Props> = (
+  props: Props
+): React.ReactElement => {
+  return (
+    <CommentBox visible={props.visible}>
+      <Input value={props.value} onChange={props.onChange}></Input>
+      <Submit onClick={props.onClick}>✓</Submit>
+    </CommentBox>
+  );
+};
+export default CommentSection;
---
+++ src/components/reusable/TextArea.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const TextArea = styled.textarea.attrs((props) => ({
+  placeholder: props.placeholder,
+  value: props.value,
+  onChange: props.onChange,
+}))`
+  color: white;
+  border: none;
+  flex: auto;
+
+  margin: 0rem 1rem;
+  padding: 1rem;
+  // min-width: 500px;
+
+  background: none;
+  box-shadow: inset 0px -2px 0 #414959;
+
+  transition: all 200ms;
+
+  &:focus {
+    outline: none;
+    background: #414959;
+  }
+
+  &::placeholder {
+    font-style: italic;
+    color: #b3c3d3;
+  }
+`;
+
+export default TextArea;
