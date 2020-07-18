d44f6898611ccfd78df4569af26f57a0da465dbc
ManpreetBhatti <manpreet@bhatti.net>
2020-07-17 21:03:37 -0400

    Update deepCopyFunction.ts
    Forgot to add this one section of code back

--- src/utils/deepCopyFunction.ts
+++ src/utils/deepCopyFunction.ts
@@ -1,6 +1,9 @@
 export const deepCopyFunction = (inObject) => {
   let value; let key;
 
+  // Create an array or object to hold the values
+  const outObject = Array.isArray(inObject) ? [] : {};
+
   if (typeof inObject !== 'object' || inObject === null) {
     return inObject; // Return the value if inObject is not an object
   }
