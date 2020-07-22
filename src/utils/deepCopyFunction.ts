2b0dd1e064414c664a504785f06430d01b3ae3a1
ManpreetBhatti <manpreet@bhatti.net>
2020-07-22 13:09:14 -0400

    Update deepCopyFunction.ts
    Fixed typescript and eslint errors

--- src/utils/deepCopyFunction.ts
+++ src/utils/deepCopyFunction.ts
@@ -1,18 +1,26 @@
-export const deepCopyFunction = (inObject) => {
-  let value; let key;
+import { Core } from '../tree/types';
 
+/**  Will be the MarkDownTree without the deletedCore's (Any core with
+ * deletedOrder > -1)
+ * @param {Core[]|Core} inObject - the whole MarkDownTree or a Core
+ * @returns {Core[]} - deep clone of the MarkDownTree
+ */
+
+export const deepCopyFunction = (inObject: Core[] | Core): Core[] => {
   // Create an array or object to hold the values
-  const outObject = Array.isArray(inObject) ? [] : {};
+  const outObject: Core[] = [];
 
   if (typeof inObject !== 'object' || inObject === null) {
     return inObject; // Return the value if inObject is not an object
   }
 
-  for (key in inObject) {
-    value = inObject[key];
-
-    // Recursively (deep) copy for nested objects, including arrays
-    outObject[key] = deepCopyFunction(value);
+  if (typeof inObject === 'object') {
+    outObject.push(Object.assign(inObject));
+  } else {
+    (inObject as Core[]).forEach((objectKey: Core) => {
+      // Recursively (deep) copy for nested objects, including arrays
+      outObject.push(...deepCopyFunction(objectKey));
+    });
   }
 
   return outObject;
