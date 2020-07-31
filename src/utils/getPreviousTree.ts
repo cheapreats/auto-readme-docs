635fe0bbfe737a2c2fa6f426420f3f7af94d15b9
sahandfarahani <58839234+sahandfarahani@users.noreply.github.com>
2020-07-30 21:17:50 -0400

    Update getPreviousTree.ts

--- src/utils/getPreviousTree.ts
+++ src/utils/getPreviousTree.ts
@@ -1,4 +1,3 @@
-import { Core } from "../tree/types";
 /**  Will Divide The treecore into path and comment
  * @param {string[]} haveComments  The exsiting treeCore in Readme File
  * @returns {Object[]} Object array of path and comment
@@ -7,15 +6,16 @@ interface oldTree {
   path: string | undefined;
   comment: string | undefined;
 }
-
+const wordsInsideParentheses = /(\(\.\/.+\))/g;
+const wordsAfterHashSymbol = /(# .+)/g;
 export const getPreviousTree = (haveComments: string[] | null): oldTree[] => {
   if (haveComments === null) {
     return [];
   }
   const haveCommentsArray: oldTree[] = [];
   haveComments.map((line, key) => {
-    const firstHalf = line.match(/(\(\.\/.+\))/g)?.toString();
-    const secondHalf = line.match(/(# .+)/g)?.toString();
+    const firstHalf = line.match(wordsInsideParentheses)?.toString();
+    const secondHalf = line.match(wordsAfterHashSymbol)?.toString();
     const path = firstHalf?.substring(1, firstHalf.length - 1);
     const comment = secondHalf?.substring(2, secondHalf.length);
     const haveCommentsOBJ = { path, comment };
