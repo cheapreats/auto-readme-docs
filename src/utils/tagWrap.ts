4e7652dab5a27a0672d5e6248551aba2f0ebdf7b
sahandfarahani <sahandfarahani@yahoo.com>
2020-08-15 13:23:30 -0400

    tagwrap JSDOC fixed

--- src/utils/tagWrap.ts
+++ src/utils/tagWrap.ts
@@ -3,7 +3,7 @@ import { WrapTagType } from "../tree/types";
 /** Gets a string and a tag and wrap the string inside the tag
  * @param {string} text text to be wrapped
  * @param {string} tag tag to wrap the text
- * @param {WrapTagType} state when we need only open the tags
+ * @param {WrapTagType} state To know whether we need the tags to be only open,close or both
  * @returns {string} the wrapped text
  */
 
