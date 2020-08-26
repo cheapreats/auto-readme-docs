c7ed8410c80cabb1949439a85958fdc04384befc
Manpreet Bhatti <manpreet@bhatti.net>
2020-08-26 14:22:01 -0400

    Update readme.config.js

--- readme.config.js
+++ readme.config.js
@@ -2,5 +2,5 @@
     "CollapsibleFolder":true,
     "RegexKeyword":"Preview",
     "Filter":"NULL",
-    "RepoLogo": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png"
+    "RepoLogo": "https://og.github.com/octicons/octicons@1200x630.png"
     }
