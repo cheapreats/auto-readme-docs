3c54ecd63fd6df897526631e74eb326d1d96d6ca
Manpreet Bhatti <manpreet@bhatti.net>
2020-08-26 14:31:10 -0400

    Update readme.config.js

--- readme.config.js
+++ readme.config.js
@@ -2,5 +2,5 @@
     "CollapsibleFolder":true,
     "RegexKeyword":"Preview",
     "Filter":"NULL",
-    "RepoLogo": "https://og.github.com/octicons/octicons@1200x630.png"
+    "RepoLogo":"https://og.github.com/octicons/octicons@1200x630.png"
     }
