9c3112bf4790aad76f35f389f699a04deea3076b
sahandfarahani <sahandfarahani@yahoo.com>
2020-08-27 15:34:37 -0400

    added Authors section

---
+++ src/utils/updateConfig.ts
@@ -0,0 +1,21 @@
+import typeValidation from "./typeValidation";
+
+/** Gets the whole decoded text and updates config object accordingly
+ * @param {string} decodedBlobs the whole decoded text
+ * @param {object} config the config object
+ * @returns {object} returns the updated config object
+ */
+
+export const updateConfig = (decodedBlobs: string, config: object): any => {
+  for (let field in config) {
+    if (typeof config[field] === "object") {
+      config[field] = updateConfig(decodedBlobs, config[field]);
+    }
+    if (typeValidation(decodedBlobs, field, typeof config[field]) !== null) {
+      config[field] = typeValidation(decodedBlobs, field, typeof config[field]);
+    }
+  }
+  return config;
+};
+
+export default updateConfig;
